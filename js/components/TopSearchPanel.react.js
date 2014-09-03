f2fcb53881a3a6b096173b3ed43d8373c5941543
Jibi Abraham <jibi.abraham@pagalguy.com>
2014-09-03 16:44:37 +0530

    So this uses something along the lines of a StateStore?

--- js/components/TopSearchPanel.react.js
+++ js/components/TopSearchPanel.react.js
@@ -3,12 +3,30 @@
  */
 
 var React = require('react');
+var ReactPropTypes = React.PropTypes;
+var MiniBranding = require("./MiniBranding.react");
+var SearchBox = require("./SearchBox.react");
+var ControlPanel = require("./ControlPanel.react");
 
 var TopSearchPanel = React.createClass({
 
+	propTypes: {
+		hasQuery: ReactPropTypes.bool.isRequired
+	},
+
+	getInitialState: function(){
+		return {
+			hasQuery: this.props.hasQuery || false
+		};
+	},
+
 	render: function() {
 		return (
-			<div />
+			<section>
+				<ControlPanel expanded={!this.state.hasQuery} />
+				<MiniBranding visible={this.state.hasQuery} />
+				<SearchBox hasQuery={this.state.hasQuery}/>
+			</section>
 		);
 	}
 
