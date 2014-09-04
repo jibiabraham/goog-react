8f33ed7846f8be3275d8f643429014cf647f3467
Jibi Abraham <jibi.abrahm@gmail.com>
2014-09-04 11:10:21 +0530

    Moving onto bootstrap -  abandonware

---
+++ js/components/SearchControls.react.js
@@ -0,0 +1,18 @@
+/**
+ * @jsx React.DOM
+ */
+
+var React = require('react');
+var ReactPropTypes = React.PropTypes;
+
+var SearchControls = React.createClass({
+
+	render: function() {
+		return (
+			<div />
+		);
+	}
+
+});
+
+module.exports = SearchControls;
